7c0c97eff254bbb20df841f151d28ac464064788
nadmit21-ux <nadmit21@gmail.com>
2026-08-16 21:54:17 +0200

    Add AnonBox service worker

---
+++ public/anonbox/sw.js
@@ -0,0 +1,4 @@
+const CACHE='anonbox-v1';
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./'])))});
+self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)))});
